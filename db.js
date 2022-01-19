9b4926f84396c9baa3ec61ac577ecc8aec46136b
Arun Pratap Singh <arunpratapsingh94@gmail.com>
2022-01-19 07:45:02 -0800

    git

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://aps_database:arunps94@cluster0.iq5mg.mongodb.net/augintern?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://aps_database:arunps94@cluster0.iq5mg.mongodb.net/Zenkart?retryWrites=true&w=majority');
