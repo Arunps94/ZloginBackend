1d0c27613e2af323c50761bad328e790b46202e9
arunpratapsingh <arunpratapsingh94@gmail.com>
2022-01-20 10:53:51 +0530

    db

--- db.js
+++ db.js
@@ -1,6 +1,4 @@
 var mongoose = require('mongoose');
-<<<<<<< HEAD
+
 mongoose.connect('mongodb+srv://aps_database:arunps94@cluster0.iq5mg.mongodb.net/Zenkart?retryWrites=true&w=majority');
-=======
-mongoose.connect('mongodb+srv://aps_database:arunps94@cluster0.iq5mg.mongodb.net/Zenkart?retryWrites=true&w=majority');
->>>>>>> 9b4926f84396c9baa3ec61ac577ecc8aec46136b
+
